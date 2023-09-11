beb4ff79d3df21ebcd46c31f95582f5647a4a39d
Abdul Samed Alhassan <129804994+abdulsamedtma@users.noreply.github.com>
2023-09-11 15:29:00 -0500

    add more photos during functionality test

--- db/schema.sql
+++ db/schema.sql
@@ -6,18 +6,19 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
+-- Create the department table
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
-
+-- Create the role table 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER
 );
-
+-- Create the employee table
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -25,3 +26,19 @@ CREATE TABLE employee (
     role_id INTEGER,
     manager_id INTEGER
 );
+
+-- Add foreign key constraints to the role table
+ALTER TABLE employee
+ADD CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
+ADD CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id);
+
+-- Additional bonus funcitonality 
+-- DELETE statements for departments, roles, and employees
+-- Delete a department by its ID
+DELETE FROM department WHERE id = ?;
+
+-- Delete a role by its ID
+DELETE FROM role WHERE id = ?;
+
+-- Delete an employee by their ID
+DELETE FROM employee WHERE id = ?;
